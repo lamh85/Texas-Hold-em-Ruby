328cdd8bad2e2e01adc0bb7029c8d3c1c65c611c
Hon Lam <lamh85@gmail.com>
2015-11-24 18:55:33 -0800

    Started building classes.

---
+++ app.rb
@@ -0,0 +1,8 @@
+class Session
+
+    require 'card.rb'
+
+    my_card = Card.new(10,2)
+    puts my_card
+
+end
---
+++ card.rb
@@ -0,0 +1,20 @@
+class Card
+
+    attr_accesor :number_strength, :suit_strength, :number_display, :suit_display
+
+    def initialize(number_strength, suit_strength)
+        @number_strength    = number_strength
+        @suit_strength      = suit_strength
+        @number_display     = number_display(number_strength)
+        @suit_display       = suit_display(suit_strength)
+    end
+
+    def number_display(strength)
+        return ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"][strength - 1]
+    end
+
+    def suit_display(strength)
+        return ["diamonds", "clubs", "hearts", "spades"][strength - 1]
+    end    
+
+end
